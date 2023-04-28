39b43416661a99afc899d1c01c5afc59e7955efa
Eren Gazioglu <pompyproduction@gmail.com>
2023-04-28 14:18:46 +0200

    Create button dialog animation

---
+++ script.js
@@ -0,0 +1,5 @@
+function handleButtonClick() {
+    document.querySelector(".add-button").classList.toggle("active")
+}
+
+document.querySelector(".add-button").addEventListener("click", handleButtonClick);
